bc6891d77fe7bcc2289690c78302d80d6f5dd0a8
melido <enrriquebello10@gmail.com>
2024-05-26 22:17:46 -0400

    (Completed) Find rating from professors with Promise.all to increase speed

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const scrapeRoute = require('./routes/scrape');
+const scrapeRoute = require('./routes/global_search/scrape');
+const rateMyProfessorRoute = require('./routes/rate_my_professor/rateMyProfessor'); // Import the new route
 const cors = require('cors');
 
 const app = express();
@@ -7,6 +8,7 @@ const PORT = 5000;
 
 app.use(cors());
 app.use('/api/scrape', scrapeRoute);
+app.use('/api/rate_my_professor', rateMyProfessorRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
